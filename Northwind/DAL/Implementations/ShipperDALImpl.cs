8015c43e4b12e90346317685da55a58d1a9cc946
monica-monterrosa <monimonte13@gmail.com>
2023-06-06 20:19:23 -0600

    Generic y GetAllCategories DAL GetAllShippers DAL

---
+++ Northwind/DAL/Implementations/ShipperDALImpl.cs
@@ -0,0 +1,62 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    internal class ShipperDALImpl : IShipperDAL
+    {
+        private NorthWindContext _northWindContext;
+        private UnidadDeTrabajo<Shipper> unidad;
+
+        public bool Add(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<Shipper> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Shipper> Find(Expression<Func<Shipper, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shipper Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Shipper> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<Shipper> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shipper SingleOrDefault(Expression<Func<Shipper, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
