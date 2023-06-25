ab0a53f121f40720a80de11522fb031ffb5e0c35
monica-monterrosa <monimonte13@gmail.com>
2023-06-25 00:26:06 -0600

    Index Category

---
+++ Northwind/FrontEnd/Helpers/CategoryHelper.cs
@@ -0,0 +1,65 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class CategoryHelper
+    {
+        ServiceRepository repository;
+
+        public CategoryHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        #region  GetALL
+
+        public List<CategoryViewModel> GetAll()
+        {
+
+
+
+            List<CategoryViewModel> lista = new List<CategoryViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Category/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<CategoryViewModel>>(content);
+
+            }
+
+
+
+
+            return lista;
+        }
+        #endregion
+
+
+        #region GetByID
+
+        /// <summary>
+        /// Obtener Categoria por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public CategoryViewModel GetByID(int id)
+        {
+            CategoryViewModel category = new CategoryViewModel();
+
+            HttpResponseMessage responseMessage = repository.GetResponse("api/category/" + id);
+            string content = responseMessage.Content.ReadAsStringAsync().Result;
+            category = JsonConvert.DeserializeObject<CategoryViewModel>(content);
+
+            return category;
+
+        }
+
+
+
+        #endregion
+
+
+
+    }
+}
---
+++ Northwind/FrontEnd/Helpers/ShipperHelper.cs
@@ -0,0 +1,61 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+namespace FrontEnd.Helpers
+{
+    public class ShipperHelper
+    {
+        ServiceRepository repository;
+
+        public ShipperHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        #region  GetALL
+
+        public List<ShipperViewModel> GetAll()
+        {
+
+
+
+            List<ShipperViewModel> lista = new List<ShipperViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Shipper/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<ShipperViewModel>>(content);
+
+            }
+
+
+
+
+            return lista;
+        }
+        #endregion
+
+
+        #region GetByID
+
+        /// <summary>
+        /// Obtener Shipper por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ShipperViewModel GetByID(int id)
+        {
+            ShipperViewModel Shipper = new ShipperViewModel();
+
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Shipper/" + id);
+            string content = responseMessage.Content.ReadAsStringAsync().Result;
+            Shipper = JsonConvert.DeserializeObject<ShipperViewModel>(content);
+
+            return Shipper;
+
+        }
+
+
+
+        #endregion
+    }
+}
