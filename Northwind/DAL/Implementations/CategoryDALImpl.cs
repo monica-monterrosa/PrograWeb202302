e79f14e06b88720dada786b8b5d50b4e447c2d4e
monica-monterrosa <monimonte13@gmail.com>
2023-07-11 20:54:38 -0600

    Sp ADD Category

--- Northwind/DAL/Implementations/CategoryDALImpl.cs
+++ Northwind/DAL/Implementations/CategoryDALImpl.cs
@@ -1,5 +1,6 @@
 ﻿using DAL.Interfaces;
 using Entities.Entities;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -19,11 +20,28 @@ namespace DAL.Implementations
         {
             try
             {
-                using (unidad = new UnidadDeTrabajo<Category>(new NorthWindContext()))
+                string sql = "exec [dbo].[sp_AddCategory] @CategoryName, @Description";
+                var param = new SqlParameter[]
                 {
-                    unidad.genericDAL.Add(entity);
-                    unidad.Complete();
-                }
+                    new SqlParameter()
+                    {
+                        ParameterName = "@CategoryName",
+                        SqlDbType= System.Data.SqlDbType.VarChar,
+                        Direction = System.Data.ParameterDirection.Input,
+                        Value= entity.CategoryName
+                    },
+                     new SqlParameter()
+                    {
+                        ParameterName = "@Description",
+                        SqlDbType= System.Data.SqlDbType.VarChar,
+                        Direction = System.Data.ParameterDirection.Input,
+                        Value= entity.Description
+                    }
+
+                };
+                NorthWindContext northWindContext = new NorthWindContext();
+
+                int resultado = northWindContext.Database.ExecuteSqlRaw(sql, param);
 
 
                 return true;
@@ -65,23 +83,30 @@ namespace DAL.Implementations
 
             string sql = "[dbo].[sp_GetAllCategories]";
             NorthWindContext northWindContext = new NorthWindContext();
-
             resultado = await northWindContext.sp_GetAllCategories_Results
-                .FromSqlRaw(sql)
-                .ToListAsync();
+                        .FromSqlRaw(sql)
+                        .ToListAsync();
 
-            foreach(var item in resultado)
+            foreach (var item in resultado)
             {
                 categories.Add(
                     new Category
                     {
-                        CategoryId= item.CategoryId,
-                        CategoryName= item.CategoryName,
-                        Description= item.Description,
-                        Picture= item.Picture,
+                        CategoryId = item.CategoryId,
+                        CategoryName = item.CategoryName,
+                        Description = item.Description,
+                        Picture = item.Picture
                     }
+
+
+
                     );
+
             }
+
+
+
+
 
             return categories;
 
