703cedd975c3a4c212724dc0a066fb03aff9421f
monica-monterrosa <monimonte13@gmail.com>
2023-07-24 23:25:29 -0600

    Product+Supplier CRUD en CONJUNTO

---
+++ Northwind/FrontEnd/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private ProductHelper productHelper;
+        private CategoryHelper categoryHelper;
+        private SupplierHelper supplierHelper;
+
+        public ProductController()
+        {
+            productHelper = new ProductHelper();
+            categoryHelper = new CategoryHelper();
+            supplierHelper = new SupplierHelper();
+        }
+
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            List<ProductViewModel> products = productHelper.GetAll();
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+
+            ProductViewModel product = productHelper.Get(id);
+
+            return View(product);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            ProductViewModel product = new ProductViewModel();
+            product.Suppliers = supplierHelper.GetAll();
+            product.Categories = categoryHelper.GetAll();
+
+            return View(product);
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductViewModel product)
+        {
+            try
+            {
+                productHelper.Create(product);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ProductViewModel product = productHelper.Get(id);
+            product.Suppliers = supplierHelper.GetAll();
+            product.Categories = categoryHelper.GetAll();
+
+            return View(product);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProductViewModel product)
+        {
+            try
+            {
+
+                productHelper.Update(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            ProductViewModel product = productHelper.Get(id);
+
+            return View(product);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(ProductViewModel product)
+        {
+            try
+            {
+                productHelper.Delete(product.ProductId);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Northwind/FrontEnd/Helpers/ProductHelper.cs
@@ -0,0 +1,103 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class ProductHelper
+    {
+        private ServiceRepository ServiceRepository;
+
+
+        public ProductHelper()
+        {
+            ServiceRepository = new ServiceRepository();
+
+        }
+
+
+
+        public List<ProductViewModel> GetAll()
+        {
+            List<ProductViewModel> lista = new List<ProductViewModel>();
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/product");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<ProductViewModel>>(content);
+            }
+
+            return lista;
+
+        }
+
+
+
+
+        public ProductViewModel Get(int id)
+        {
+            ProductViewModel ProductViewModel;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/product/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            ProductViewModel = JsonConvert.DeserializeObject<ProductViewModel>(content);
+
+
+
+            return ProductViewModel;
+        }
+
+
+        public ProductViewModel Create(ProductViewModel product)
+        {
+
+
+            ProductViewModel ProductViewModel;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.PostResponse("api/product/", product);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            ProductViewModel = JsonConvert.DeserializeObject<ProductViewModel>(content);
+
+
+
+            return ProductViewModel;
+        }
+
+
+        public ProductViewModel Update(ProductViewModel product)
+        {
+
+
+            ProductViewModel ProductViewModel;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.PutResponse("api/product/", product);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            ProductViewModel = JsonConvert.DeserializeObject<ProductViewModel>(content);
+
+
+
+            return ProductViewModel;
+        }
+
+
+
+        public ProductViewModel Delete(int id)
+        {
+
+
+            ProductViewModel Product;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.DeleteResponse("api/product/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            Product = JsonConvert.DeserializeObject<ProductViewModel>(content);
+
+
+
+            return Product;
+        }
+    }
+}
